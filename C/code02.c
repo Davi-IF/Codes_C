4206acec527daa237ff9e4972d99ec63d7c53c11
Davi_Matheus <dmnm2@aluno.ifal.edu.br>
2024-11-26 12:10:40 -0300

    new code 02 (LP)

---
+++ C/code02.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int main () {
+
+    float price, discount, tot_discount, final_value;
+
+    printf("what's value of product:");
+    scanf("%f", &price);
+    printf("what's value of discount:");
+    scanf("%f", &discount);
+
+    tot_discount = price * (discount/100);
+    final_value = price - tot_discount;
+
+    printf(" the final value: %2.2f ", final_value);
+
+}
