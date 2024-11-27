801cd78011f7debde1e4efbaa96008bc71c1cedb
Davi_Matheus <dmnm2@aluno.ifal.edu.br>
2024-11-27 13:16:52 -0300

    created cod5.c(LP)

---
+++ C/cod5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+int main () {
+
+    int cig_day, qtd_days, lost_days;
+    float years, tot_cig, lost_minutes;
+
+    printf("type how many cigarettes for day: ");
+    scanf("%i", &cig_day);
+    printf("type how many years somke:");
+    scanf("%f", &years);
+
+    qtd_days = years * 365;
+    tot_cig = qtd_days * cig_day;
+    
+    lost_minutes = tot_cig * 10;
+    lost_days = lost_minutes / 1440;
+
+    printf("lost days: %i dias", lost_days);
+
+
+}
