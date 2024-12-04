daff475dd3c5f76d4a8b7f2ef740bcf5d3d4522d
Davi_Matheus <dmnm2@aluno.ifal.edu.br>
2024-12-04 17:26:06 -0300

    created cod4

---
+++ C/att4(LP)/cod4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main () {
+
+    int v_house, salary, yearsp;
+
+    printf("type the value of house:");
+    scanf("%i", &v_house);
+    printf("type the salary:");
+    scanf("%i", &salary);
+    printf("type the yearsp:");
+    scanf("%i", &yearsp);
+
+
+
+}
